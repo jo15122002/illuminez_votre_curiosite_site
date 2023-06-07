c8038356cb8049e92c9e891c8be0fc54e13960d9
JoMobile <lapinette2002@ymail.com>
2023-06-07 14:37:12 +0200

    Style des boutons

--- app.vue
+++ app.vue
@@ -8,15 +8,19 @@
   </div>
 
   <div class="flex-row content-div">
-    <div class="flex-column half-width">
-      <h4 class="date">Exposition de mai à décembre 2023</h4>
-      <h1>Illuminez votre curiosité</h1>
-      <p>La bioluminescence est une réaction chimique que produisent les êtres vivants pour différents usage.
-        Proposez à vos enfants une nouvelle façon de découvrir et s’instruire autour des espèces bioluminescentes, 
-        en les laissant en autonomie lors d’une exposition interactif.</p>
-      <div class="flex-row">
-        <div class="button">Acheter un ticket</div>
-        <div class="button">Vidéo de l'exposition</div>
+    <div class="flex-column half-width description-container">
+      <div class="description">
+        <h4 class="date">Exposition de juin à décembre 2023</h4>
+        <h1>Illuminez votre curiosité</h1>
+        <div class="bloc-content">
+          <p>La bioluminescence est une réaction chimique que produisent les êtres vivants pour différents usage.
+          Proposez à vos enfants une nouvelle façon de découvrir et s’instruire autour des espèces bioluminescentes, 
+          en les laissant en autonomie lors d’une exposition interactif.</p>
+          <div class="flex-row buttons-container">
+            <a href="#buySection" class="button buyButton">Acheter un ticket</a>
+            <a href="#video" class="button videoButton">Vidéo de l'exposition &#x2198;</a>
+          </div>
+        </div>
       </div>
     </div>
     <div class="carouselContainer">
@@ -33,8 +37,9 @@
 @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@600&display=swap');
 .navbar{
   margin: auto;
+  margin-top: 3%;
   width: 90vw;
-  height: 14vh;
+  height: 10vh;
   border-radius: 20px;
   background-color: #C5E2A6;
   display: flex;
@@ -44,11 +49,32 @@
 
 .content-div{
   margin: auto;
+  padding-top: 2.5%;
   width: 90vw;
+  color: #02295F;
 }
+
+.bloc-content{
+  width: 35vw;
+}
+
+.buttons-container{
+  justify-content: space-between;
+}
+
 .carouselContainer{
   width: 50%;
-  height: 877px;
+  height: 100vh;
+}
+
+.description{
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+.description-container{
+  display: flex;
+  height: 100vh;
 }
 
 .navbar-items-container{
@@ -67,12 +93,6 @@
   font-family: 'Red Hat Display', sans-serif;
 }
 
-.page {
-  width: 100vw;
-  height: 100vh;
-  background-color: #FEFCF8;
-}
-
 .flex-column{
   display: flex;
   flex-direction: column;
@@ -84,11 +104,32 @@
 }
 
 .button{
-  width: 33%;
-  height: 30px;
-  border-radius: 36px;
-  background-color: #3AA098;
+  text-decoration: none;
+  display: flex;
+  width: auto;
+  height: auto;
+  border-radius: 3.4vw;
   text-align: center;
+  font-family: "Red Hat Display SemiBold", sans-serif;
+  justify-content: center;
+  align-items: center;
+  padding-right: 1.5vw;
+  padding-left: 1.5vw;
+
+  padding-bottom: 1.8vh;
+  padding-top: 1.8vh;
+
+  font-size: 1.5vw;
+}
+
+.buyButton{
+  background-color: #3AA098;
+  color: white;
+}
+
+.videoButton{
+  background-color: #C5E2A6;
+  color: #02295F;
 }
 
 .half-width{
@@ -97,11 +138,13 @@
 
 .content-div .date{
   font-family: "Red Hat Display Bold", sans-serif;
+  font-size: 1.5vw;
 }
 
 .content-div h1{
   font-family: "BasteleurBold";
-  font-size: 3.5vw;
+  font-size: 6vw;
+  margin-top: 1%;
 }
 
 .content-div p{
