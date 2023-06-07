7fe4ec47884598732303ee09566b4ff67f507647
JoMobile <lapinette2002@ymail.com>
2023-06-07 11:28:13 +0200

    création du carousel

---
+++ components/FishCarousel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="carousel">
+        <FishCarouselLine :reverse="true"/>
+        <FishCarouselLine :reverse="false"/>
+        <FishCarouselLine :reverse="true"/>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .carousel {
+        width: 100%;
+        overflow: hidden;
+        position: relative;
+    }
+</style>
---
+++ components/FishCarouselLineElement.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="element">
+        <img src="https://cataas.com/cat/says/hi" alt="">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .element {
+        height: 100%;
+        aspect-ratio: 1/1;
+        background-color: #f0f0f0;
+        border-radius: 20px;
+    }
+
+    .element img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 20px;
+    }
+</style>
