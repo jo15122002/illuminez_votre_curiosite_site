c4523848a242511135b79592ada10f41f2409be0
Joyce <lapinette2002@ymail.com>
2023-06-25 14:48:09 +0200

    fix fishcarousel

--- components/FishCarousel.vue
+++ components/FishCarousel.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="carousel">
-        <FishCarouselLine :reverse="true"/>
         <FishCarouselLine :reverse="false"/>
-        <FishCarouselLine :reverse="true"/>
+        <FishCarouselLine :reverse="false"/>
+        <FishCarouselLine :reverse="false"/>
     </div>
 </template>
 
 <script>
+import FishCarouselLine from '~/components/FishCarouselLine.vue';
 
+export default {
+  components: {
+    FishCarouselLine
+  },
+}
 </script>
 
 <style scoped>
@@ -16,5 +22,8 @@
         height: 100%;
         overflow: hidden;
         position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
     }
 </style>
