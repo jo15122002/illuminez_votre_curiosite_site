3cc279afb6fa45d66c2bdc499ad23df1acf8bfbf
JoMobile <lapinette2002@ymail.com>
2023-06-26 19:04:29 +0200

    test fix font

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+@font-face {
+    font-family: 'BasteleurBold';
+    src: url("~/public/Basteleur-Bold.ttf");
+    font-weight: bold;
+    font-style: normal;
+}
+</style>
