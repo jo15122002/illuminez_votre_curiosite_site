54f07bb5abff624bdeba6e488ce75de793cc1f5f
JoMobile <lapinette2002@ymail.com>
2023-06-26 19:27:11 +0200

    test fix font

--- app.vue
+++ app.vue
@@ -7,7 +7,10 @@
 <style>
 @font-face {
     font-family: 'BasteleurBold';
-    src: url("~/public/Basteleur-Bold.ttf");
+    src: url("~/assets/fonts/Basteleur-Bold.otf") format("opentype"),
+         url("~/assets/fonts/Basteleur-Bold.woff2") format("woff2"),
+         url("~/assets/fonts/Basteleur-Bold.woff") format("woff"),
+         url("~/assets/fonts/Basteleur-Bold.ttf") format("truetype");
     font-weight: bold;
     font-style: normal;
 }
